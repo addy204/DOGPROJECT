0cb32896f854180751f38388581aa45b05f6997a
addy204 <aksharma@academic.rrc.ca>
2024-07-03 19:42:56 -0500

    Implement owner-breed relationship, add search and filter functionality, and update views

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,6 +1,16 @@
 class BreedsController < ApplicationController
   def index
-    @breeds = Breed.page(params[:page]).per(10)
+    @breeds = Breed.all
+
+    if params[:query].present?
+      @breeds = @breeds.where('name LIKE ?', "%#{params[:query]}%")
+    end
+
+    if params[:owner_id].present?
+      @breeds = @breeds.joins(:owners).where(owners: { id: params[:owner_id] }).distinct
+    end
+
+    @breeds = @breeds.page(params[:page]).per(10)
   end
 
   def show
